6d1e45c9e1262b473f6dde57c9c5e257387e7331
rowmahn <dulalroman1@gmail.com>
2023-09-05 15:53:48 +0800

    Input 30 different marks and displayed

--- AssessmentStatistics.java
+++ AssessmentStatistics.java
@@ -18,13 +18,34 @@ public class AssessmentStatistics
     
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
+        float[] markList = new float [30];
         
         //Read Assignment
         System.out.println ("Enter your assignment name: ");
         String assignmentName = sc.nextLine();
         
-        //Display assignment name
-        System.out.println ("Your assignment name is "+assignmentName);
+        //Input 30 different marks
+        for (int counter=0; counter<30; counter++){
+            System.out.println ("Enter marks for student "+(counter+1));
+            float mark = sc.nextFloat();
+            
+            if (isValidMark(mark)){
+                markList[counter]=mark;
+            }
+            else {
+                System.out.println ("Invalid mark. Please enter a valid mark between 0 and 30. ");
+                counter--;
+            }
+        }
         
+        //Display assignment name & Students' mark
+        System.out.println ("Your assignment name is "+assignmentName);
+        System.out.println ("Students' Mark: "+Arrays.toString(markList));
+        
+    }
+    
+    //Function to check the valid marks between 0 & 30.
+    public static boolean isValidMark(float mark) {
+        return mark >= 0 && mark <= 30;
     }
 }
