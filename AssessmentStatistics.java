7df46285ddbf568ce93abfd436118f81fd00d60b
rowmahn <dulalroman1@gmail.com>
2023-09-05 15:24:15 +0800

    Initial commit

---
+++ AssessmentStatistics.java
@@ -0,0 +1,17 @@
+
+/**
+ * Write a description of class AssessmentStatistics here.
+ *
+ * @author (Roman Dulal)
+ * @student Id (24227472)
+ * @version (02/09/23)
+ */
+public class AssessmentStatistics
+{
+    
+    public AssessmentStatistics()
+    {
+        
+    }
+
+}
